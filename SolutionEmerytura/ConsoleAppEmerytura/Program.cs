a0c1e43d816124847b5807b07b605802f54e8de8
gbochniak <55148785+gbochniak@users.noreply.github.com>
2019-09-10 18:26:49 +0200

    wersja 1

---
+++ SolutionEmerytura/ConsoleAppEmerytura/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEmerytura
+{
+    class Program
+    {
+        const int wiekEmerytalny = 67;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplikacja emerytura");
+
+            Console.Write("Podaj imie: ");
+            string imie = Console.ReadLine();
+            Console.WriteLine($"Witaj {imie}");
+            Console.Write("Podaj wiek: ");
+            int wiek = int.Parse(Console.ReadLine());
+            if (wiek < wiekEmerytalny)
+            {
+                Console.WriteLine($"Zostalo Ci {wiekEmerytalny-wiek} lat do emerytury");
+            }
+            else
+            {
+                Console.WriteLine("Jestes emerytem");
+            }
+
+            //Console.ReadKey();
+
+
+            
+        }
+    }
+}
